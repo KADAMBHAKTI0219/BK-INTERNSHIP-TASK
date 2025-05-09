c3dcd8039d2963ccb1d85914bc52055824bb6ba6
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-05-09 10:46:49 +0530

    completed

---
+++ palmistry-app/src/app/page.js
@@ -0,0 +1,10 @@
+import WebcamCapture from "@/components/WebCamCapture";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <main className="min-h-screen bg-gray-100">
+      <WebcamCapture />
+    </main>
+  );
+}
